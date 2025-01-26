35328ec5227ebb4c0a946c3e1cef903652c9237a
AnupamaCodippily <avcodippily@gmail.com>
2025-01-27 01:13:28 +0530

    remove log

--- renderer/utils/load-exam.ts
+++ renderer/utils/load-exam.ts
@@ -2,8 +2,5 @@ import { Exam } from "../models/Exam";
 
 export function loadExamFromJsonString(data: string): Exam {
     const exam = JSON.parse(data) as Exam;
-
-    console.log(exam)
-
     return exam;
 }
